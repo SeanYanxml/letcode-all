973f2baf9283a6fef56d5cfc76188e6cfd8e6dd5
huaianyanxu <huaianyanxu@163.com>
2021-03-02 00:13:14 +0800

    更新a303说明.

--- algorithm/src/main/java/com/yanxml/letcode/algorithm/a303/SumRange2.java
+++ algorithm/src/main/java/com/yanxml/letcode/algorithm/a303/SumRange2.java
@@ -6,16 +6,28 @@ package com.yanxml.letcode.algorithm.a303;
  */
 
 public class SumRange2 {
-	public int sums[] = null;
+	 // 动态规划
+		public int sums[] = null;
 
-	public SumRange2(int[] nums) {
-	   sums[0] = nums[0];
-	   for(int i = 1; i <= nums.length; i++) {
-		   sums[i] = sums[i-1] + nums[i];
-	   }
-	}
-
-	public int sumRange(int i, int j) {
-		return sums[j]-sums[i];
-	}
+	    public SumRange2(int[] nums) {
+	       sums = new int[nums.length];
+	       if(nums.length >0){
+	            sums[0] = nums[0];
+		        for(int i = 1; i < nums.length; i++) {
+			        sums[i] = sums[i-1] + nums[i];
+		        }
+	       }	  
+	    }
+	    
+	    // 注意是计算i-j 包含nums[i] nums[j]
+	    // sums[j]-sums[i] 会导致缺少nums[i] 所以一般计算 sums[j] - sums[i-1]
+	    // 为此又要考虑i-1为0的情况.
+	    // -2 0 1 -4 -2 -3
+	    public int sumRange(int i, int j) {
+	        if(i==0){
+	            return sums[j];
+	        }else{
+	            return sums[j] - sums[i-1];
+	        }
+	    }
 }
